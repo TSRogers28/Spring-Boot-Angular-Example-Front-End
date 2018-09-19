20248ef56e4768eea03f537ac2bd7fda7e74a573
Thomas Rogers <tsrjr@udel.edu>
2018-09-19 14:14:18 -0400

    added component beer-list

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BeerListComponent
   ],
   imports: [
     BrowserModule
